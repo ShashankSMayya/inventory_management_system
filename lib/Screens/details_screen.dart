605dba6b666f1b3c82c066dae724c3a8688b1ed2
Kannan <kannanpadmam2014@gmail.com>
2020-10-17 06:23:28 +0530

    product details fetched and stored in variables

--- lib/Screens/details_screen.dart
+++ lib/Screens/details_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DetailsScreen extends StatefulWidget {
   final String productId, msg;
@@ -9,6 +12,12 @@ class DetailsScreen extends StatefulWidget {
 
 
 class _DetailsScreenState extends State<DetailsScreen> {
+  var product_name;
+  var product_id;
+  var product_brand;
+  var product_cat;
+  var product_desc;
+  var product_image;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,4 +40,30 @@ class _DetailsScreenState extends State<DetailsScreen> {
       ),
     );
   }
+
+  Future getData() async{
+    var id = await getStringValuesSF('product_id');
+    String body =json.encode(id);
+    var url = 'https://demo121flutter.000webhostapp.com/product_details.php';//
+    http.Response response = await http.post(url, body:body);
+    Map data = jsonDecode(response.body);
+    product_name = data["product_title"];
+    product_id = data["product_id"];
+    product_brand = data["product_brand"];
+    product_cat = data["product_cat"];
+    product_desc = data["product_desc"];
+    product_image = data["product_image"];
+  }
+}
+
+
+
+getStringValuesSF(data) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String stringValue = await prefs.getString(data.toString()) ?? 'blank value';
+  return stringValue;
+}
+addStringToSF(key,value) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setString(key.toString(), value.toString());
 }
